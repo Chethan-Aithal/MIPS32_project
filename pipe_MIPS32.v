56e6511950aab1cf77ea41f22a822b430f6a3c8d
Chethan-Aithal <chethanaithal25@gmail.com>
2026-04-24 21:34:48 +0530

    Update pipe_MIPS32.v

--- pipe_MIPS32.v
+++ pipe_MIPS32.v
@@ -20,33 +20,24 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module pipe_MIPS32(clk1, clk2);
-
 input clk1, clk2;
-
 reg [31:0] PC, IF_ID_IR, IF_ID_NPC;
 reg [31:0] ID_EX_IR, ID_EX_NPC, ID_EX_A, ID_EX_B, ID_EX_Imm;
 reg [2:0] ID_EX_type, EX_MEM_type, MEM_WB_type;
-
 reg [31:0] EX_MEM_IR, EX_MEM_ALUOut, EX_MEM_B;
 reg EX_MEM_cond;
-
 reg [31:0] MEM_WB_IR, MEM_WB_ALUOut, MEM_WB_LMD;
-
 reg [31:0] Reg [0:31];
 reg [31:0] Mem [0:1023];
-
 parameter ADD=6'b000000, SUB=6'b000001, AND=6'b000010, OR=6'b000011,
           SLT=6'b000100, MUL=6'b000101, HLT=6'b111111,
           LW=6'b001000, SW=6'b001001,
           ADDI=6'b001010, SUBI=6'b001011, SLTI=6'b001100,
           BNEQZ=6'b001101, BEQZ=6'b001110;
-
 parameter RR_ALU=3'b000, RM_ALU=3'b001, LOAD=3'b010,
           STORE=3'b011, BRANCH=3'b100, HALT=3'b101;
 
 reg HALTED, TAKEN_BRANCH;
-
-
 // IF
 always @(posedge clk1) begin
     if(!HALTED) begin
@@ -63,8 +54,6 @@ always @(posedge clk1) begin
         end
     end
 end
-
-
 // ID
 always @(posedge clk2) begin
     if(!HALTED) begin
@@ -86,8 +75,6 @@ always @(posedge clk2) begin
         endcase
     end
 end
-
-
 // EX
 always @(posedge clk1) begin
     if(!HALTED) begin
@@ -106,7 +93,6 @@ always @(posedge clk1) begin
                     MUL: EX_MEM_ALUOut<=#2 ID_EX_A*ID_EX_B;
                 endcase
             end
-
             RM_ALU: begin
                 case(ID_EX_IR[31:26])
                     ADDI: EX_MEM_ALUOut<=#2 ID_EX_A+ID_EX_Imm;
@@ -114,21 +100,17 @@ always @(posedge clk1) begin
                     SLTI: EX_MEM_ALUOut<=#2 (ID_EX_A<ID_EX_Imm);
                 endcase
             end
-
-            LOAD,STORE: begin
+LOAD,STORE: begin
                 EX_MEM_ALUOut<=#2 ID_EX_A+ID_EX_Imm;
                 EX_MEM_B<=#2 ID_EX_B;
             end
-
-            BRANCH: begin
+BRANCH: begin
                 EX_MEM_ALUOut<=#2 ID_EX_NPC+ID_EX_Imm;
                 EX_MEM_cond<=#2 (ID_EX_A==0);
             end
         endcase
     end
 end
-
-
 // MEM
 always @(posedge clk2) begin
     if(!HALTED) begin
@@ -142,8 +124,6 @@ always @(posedge clk2) begin
         endcase
     end
 end
-
-
 // WB
 always @(posedge clk1) begin
     if(!TAKEN_BRANCH) begin
@@ -155,5 +135,4 @@ always @(posedge clk1) begin
         endcase
     end
 end
-
 endmodule
